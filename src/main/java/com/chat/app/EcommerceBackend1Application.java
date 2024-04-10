7db91b3ae3312d2aa4a2abfebe1a007bed7187e2
Gaurav Jain <78154992+gjain87@users.noreply.github.com>
2024-04-10 12:00:06 +0545

    Add files via upload

---
+++ src/main/java/com/chat/app/EcommerceBackend1Application.java
@@ -0,0 +1,13 @@
+package com.chat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackend1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat/app/EcommerceBackend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackend1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
